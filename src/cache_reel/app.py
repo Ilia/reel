25344a1a45f2387c4b02d335178cd26f1b818efd
Ilia Mogilevsky <ilia.mogilevsky@astutepayroll.com>
2021-09-06 08:02:48 +1000

    Adding filtering for ID, adding try catches

--- src/cache_reel/app.py
+++ src/cache_reel/app.py
@@ -10,11 +10,9 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-
-    logger.info('event: %s', event)
-
-    # TODO: ensure we have proper validation
-    id = event['Records'][0]['Sns']['Message']
+    
+    id = getId(event)
+    
     data = fetchDataFromApi(id)
     
     if(str(data['id']) != str(id)):
@@ -24,6 +22,23 @@ def lambda_handler(event, context):
     
     return response(body=json.dumps(data))
 
+
+def getId(event):
+    id = None
+
+    if 'reel_id' in event:
+        id = event['reel_id']
+
+    if 'Records' in event:
+        try:
+            id = event.get('Records')[0].get('Sns').get('Message')
+        except KeyError as e:
+            raise ValueError('Invalid SNS message.')
+
+    if id is None:
+        raise ValueError('Invalid Reel ID.')
+
+    return id
 
 def formatData(data):
     # TODO: format data, we dont need to store everything in the cache, just what we need 
@@ -36,10 +51,14 @@ def upsert(id, data):
         'reel_id': {'S': str(id)},
         'data': {'S': json.dumps(data)},
     }
-    return dynamodb.put_item(
-        TableName=table_name,
-        Item=params
-    )
+    try:
+        return dynamodb.put_item(
+            TableName=table_name,
+            Item=params
+        )
+    except (ClientError, ParamValidationError) as e:
+        logging.error(e)
+        raise
     
 def fetchDataFromApi(id):
     api_endpoint = os.environ.get('API_ENDPOINT')
