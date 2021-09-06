6f0b0e3580a36429104e722b3cfe4e052ff0e78c
Ilia Mogilevsky <ilia.mogilevsky@astutepayroll.com>
2021-09-07 08:46:54 +1000

    Adding simple test lambda to test the tests :)

---
+++ src/test/app.py
@@ -0,0 +1,14 @@
+import json
+import os
+import boto3
+
+# from common import response
+
+def lambda_handler(event, context):
+    return response()
+
+def response():
+    return {
+        'statusCode': 200,
+        'body': 'Ok'
+    }
