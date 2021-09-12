c41c81d227ded5b0e94e82b706513c5a72e3cafd
Ilia Mogilevsky <ilia.mogilevsky@astutepayroll.com>
2021-09-12 17:26:40 +1000

    Restructure to handle errors better

--- src/cache_all/app.py
+++ src/cache_all/app.py
@@ -3,26 +3,37 @@ import requests
 import boto3
 import os
 import logging
+
 from common import response
 
 client = boto3.client('sns') 
 logger = logging.getLogger('boto3')
 
 def lambda_handler(event, context):
-    request = requests.get(os.environ['API_ENDPOINT'], 
-        params={'access_token':os.environ['API_TOKEN']})
+    try:
+        json_data = fetch_reels()
+        print (json_data)
+    except requests.exceptions.RequestException as e: 
+        logging.critical(e, exc_info=True) 
+        return response(code=500, body="Failed getting response from Reel API")
 
-    json_data = request.json()
-    
+    if 'response' not in json_data:
+        return response(code=400, body="Could not get proper response from Reel API")
+
     for data in json_data['response']:
-        cacheReel(id=int(data['id']))
+        cache_reel(id=int(data['id']))
     
     return response()
 
-def cacheReel(id):   
-    response = client.publish(
+def cache_reel(id):   
+    return client.publish(
         TargetArn=os.environ['CACHE_TOPIC_ARN'],
         Message=json.dumps({'default': json.dumps(id)}),
         MessageStructure='json'
     )
-    logger.info('SNS response: %s', response)
+
+def fetch_reels():
+    request = requests.get(os.environ['API_ENDPOINT'], 
+        params={'access_token':os.environ['API_TOKEN']})
+
+    return request.json()
